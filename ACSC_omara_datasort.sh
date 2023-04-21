9bc47c9987253a3da98ffeaa11056634c02ff185
Ada Quinn <uqadaqu1@uq.edu.au>
2023-04-21 16:35:37 +1000

    initial commit

---
+++ ACSC_omara_datasort.sh
@@ -0,0 +1,215 @@
+#!/bin/bash
+
+# Default values
+
+extra_itp=()
+dry_run=false
+notrj=false
+
+# Parse command line arguments
+ARGS=$(getopt -o i:p:o:f::r:x: --long input_sysname:,input_path:,output_sysname:,ff_path:,rep:,extra_itp:,dry-run:,notrj -- "$@")
+
+#ARGS=$(getopt -o i:p:o:f::r:x: --long input_sysname:,input_path:,output_sysname:,ff_path:,rep:,extra_itp:,dry-run: -- "$@")
+eval set -- "$ARGS"
+
+# Process options and their arguments
+while true; do
+  case "$1" in
+    -i|--input_sysname)
+      input_sysname="$2"
+      shift 2
+      ;;
+    -p|--input_path)
+      input_path="$2"
+      shift 2
+      ;;
+    -o|--output_sysname)
+      output_sysname="$2"
+      shift 2
+      ;;
+    -f|--ff_path)
+      ff_path="$2"
+      shift 2
+      ;;
+    -r|--rep)
+      rep="$2"  # doesn't do anything right now
+      shift 2
+      ;;
+    -x|--extra_itp)
+      extra_itp+=("$2")
+      shift 2
+      ;;
+    --dry-run)
+      dry_run=true
+      shift
+      ;;
+    --notrj)
+      notrj=true
+      shift
+      ;;
+    # --trj)
+    #   trj=true
+    #   shift
+    #   ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      echo "Invalid option: $1"
+      exit 1
+      ;;
+  esac
+done
+
+
+# check that variables are set
+if [ -z "$input_sysname" ] ; then
+  echo "Error: --input_sysname not specified"
+  exit 1
+fi
+
+if [ -z "$input_path" ] ; then
+  echo "Error: --input_path not specified"
+  exit 1
+fi
+
+if [ -z "$output_sysname" ] ; then
+  echo "Error: --output_sysname not specified"
+  exit 1
+fi
+
+
+if [ -z "$ff_path" ] ; then
+  echo "Error: --forcefield_folder not specified"
+  exit 1
+fi
+
+# Check that forcefield_folder exists
+if [ ! -d "$ff_path" ]; then
+  echo "Error: forcefield_folder '$ff_path' does not exist"
+  exit 1
+fi
+
+
+# build the 
+
+
+
+# Output the values of the variables
+echo "Input sysname: $input_sysname"
+echo "Input filepath: $input_path"
+echo "Output sysname: $output_sysname"
+echo "trj:" $trj 
+if [ -n "$rep" ]; then
+  echo "Rep: $rep"
+fi
+echo "Forcefield folder path: $ff_path"
+if [ ${#extra_itp[@]} -gt 0 ]; then
+  echo "Extra ITP files: ${extra_itp[@]}"
+fi
+if [ "$dry_run" = true ]; then
+  echo "Dry run; exiting now"
+  exit 0
+fi
+# if [ "$trj" = true ]; then
+#   echo "--trj specified; trajectory files will be copied"
+# fi
+echo
+echo "Beginning system copy"
+
+if [ -d "$output_sysname" ]; then
+  echo "ALERT: directory '$output_sysname' already exists"
+  echo
+fi
+
+
+mkdir ${output_sysname} -p
+
+cat <<EOF > ${output_sysname}/atbrepo.yaml
+title: "This will appear as the title of the simulation on the ACSC website. Should be enclosed in quotation marks."
+notes: "This will appear as a description of the simulation on the ACSC website. Should be enclosed in quotation marks.  If the data is related to a publication, the DOI of the publication can also be included in this field."
+program: GROMACS
+organization: omara
+tags: Freeform text tags for the simulation, prefixed with a hyphen. Note that some tags are prefixed with "item-", as shown below
+    - replicate-[number] of [total number]
+    - protein-[name of protein]
+    - peptide-[name of peptide]
+    - lipid-[name of lipid]
+    - membrane-[type of membrane]
+    - PDB-[pdb code]
+    - solvent-[name of solvent]
+EOF
+
+# copy control files
+
+mkdir ${output_sysname}/control -p
+cp ${input_path}/${input_sysname}.mdp ${output_sysname}/control/${output_sysname}_control_00001.mdp
+
+# copy energy files if any exist
+
+mkdir ${output_sysname}/energy -p
+if [ -f "${input_path}/${input_path}.edr" ]; then 
+    cp ${input_path}/${input_sysname}.edr ${output_sysname}/energy/${output_sysname}_energy_00001.edr
+fi
+
+# copy final coordinates if any exist
+mkdir ${output_sysname}/final-coordinates -p
+if [ -f "${input_path}/${input_sysname}.gro" ]; then 
+    cp ${input_path}/${input_sysname}.gro ${output_sysname}/final-coordinates/${output_sysname}_final-coordinates_00001.gro
+fi
+
+# copy input coordinates if any exist
+mkdir ${output_sysname}/input-coordinates -p
+if [ -f "${input_path}/${input_sysname}_start.gro" ]; then 
+    cp ${input_path}/${input_sysname}_start.gro ${output_sysname}/input-coordinates/${output_sysname}_input-coordinates_00001.gro
+fi
+
+# copy forcefield files itp files etc)
+mkdir ${output_sysname}/forcefield-files -p
+cp -r ${ff_path} ${output_sysname}/forcefield-files/${output_sysname}_forcefield-files.ff
+
+# grab extra itp files if any were specified, like the protein.itp you get from go_martinize, add them to the forcefield files
+if [ ${#extra_itp[@]} -gt 0 ]; then
+  for $itp in ${extra_itp[@]}; do
+  cp $itp  ${output_sysname}/forcefield-files/${output_sysname}_forcefield-files.ff/
+  done
+fi
+
+# copy log file if it exists
+mkdir ${output_sysname}/log -p
+if [ -f "${input_path}/${input_sysname}.log" ]; then 
+    cp ${input_path}/${input_sysname}.log ${output_sysname}/input-coordinates/${output_sysname}_log_00001.log
+fi
+
+# make a reference coordinates folder (required)
+# user will have to add any reference coordinates manually for now
+mkdir ${output_sysname}/reference-coordinates -p
+
+# copy topology file
+mkdir ${output_sysname}/topology -p
+cp ${input_path}/${input_sysname}.top ${output_sysname}/topology/${output_sysname}_topology_00001.top
+
+mkdir ${output_sysname}/trajectory -p # folder is required regardless of whether trajectories are included
+# copy trajectories unless --notrj was specified
+if [ "$notrj" = false ]; then
+    files=false
+    if [ -f "${input_path}/${input_sysname}.xtc" ]; then 
+        echo "copying trajectory file ${input_path}/${input_sysname}.xtc"
+        echo
+        rsync --progress ${input_path}/${input_sysname}.xtc ${output_sysname}/trajectory/${output_sysname}_trajectory_00001.xtc # use rsync for a progress bar and for data fidelity; trajectories are big
+        files=true
+    fi
+    if [ -f "${input_path}/${input_sysname}.trr" ]; then 
+        echo "copying trajectory file ${input_path}/${input_sysname}.trr"
+        echo "ALERT: MAKE SURE YOU ACTUALLY WANT THIS, .trr FILES ARE USUALLY ENOURMOUS"
+        echo
+        rsync --progress ${input_path}/${input_sysname}.trr ${output_sysname}/trajectory/${output_sysname}_trajectory_00001.trr # use rsync for a progress bar and for data fidelity; trajectories are big
+        files=true
+    fi
+    if files=false; then
+        "ALERT: no trajectory files were found with file names ${input_path}/${input_sysname}.(xtc|trr)"
+    fi
+else
+    echo "--notrj specified, trajectories not copied"
+fi
