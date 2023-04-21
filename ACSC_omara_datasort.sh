1a51e5d036b9e840fea511ca331d7ceb36052cfd
Ada Quinn <uqadaqu1@uq.edu.au>
2023-04-21 17:01:52 +1000

    added documentation

--- ACSC_omara_datasort.sh
+++ ACSC_omara_datasort.sh
@@ -5,9 +5,9 @@
 extra_itp=()
 dry_run=false
 notrj=false
-
+help=false
 # Parse command line arguments
-ARGS=$(getopt -o i:p:o:f::r:x: --long input_sysname:,input_path:,output_sysname:,ff_path:,rep:,extra_itp:,dry-run:,notrj -- "$@")
+ARGS=$(getopt -o hi:p:o:f::r:x: --long input_sysname:,input_path:,output_sysname:,ff_path:,rep:,extra_itp:,dry-run:,notrj -- "$@")
 
 #ARGS=$(getopt -o i:p:o:f::r:x: --long input_sysname:,input_path:,output_sysname:,ff_path:,rep:,extra_itp:,dry-run: -- "$@")
 eval set -- "$ARGS"
@@ -47,6 +47,11 @@ while true; do
       notrj=true
       shift
       ;;
+    -h)
+      help=true
+      shift
+      ;;
+      
     # --trj)
     #   trj=true
     #   shift
@@ -62,7 +67,40 @@ while true; do
   esac
 done
 
-
+if [ "$help" = true ]; then
+  echo "script to copy a simulation replicate into a dataset with the directory structure"
+  echo "suitable for the atb acsc repository at https://molecular-dynamics.atb.uq.edu.au"
+  echo ""
+  echo "options:"
+  echo ""
+  echo "  -i or --input_sysname  : the system name of the simulation replicate.  Required."
+  echo "                           Input files should have names like input_sysname.mdp,"
+  echo "                           input_sysname.tpr, input_sysname.top, etc, etc "
+  echo ""
+  echo "  -p or --input_path     : the path to the simulation replicate.  Required."
+  echo ""
+  echo "  -o or --output_sysname : System name to be used by the ATB ACSC repository. Required."
+  echo "                           This must be unique.  Our standard structure is "
+  echo "                           OMara_[Detailed-project-name-and-year]_[Detailed-system-name]_r[number]"
+  echo "                           for example, replicate one of the ring oxidised cholesterol system from"
+  echo "                           our Neuronal OxChol paper doi.org/10.1021/acschemneuro.1c00395"
+  echo "                           would have the name: OMara_NeuronalOxChol-2021_ringOHC_r1"
+  echo ""
+  echo "  -f or --ff_path        : path to the folder containing the majority of .itp files. Required."
+  echo ""
+  echo "  -r or --rep            : replicate number.  Doesn't do anything right now. Optional."
+  echo ""
+  echo "  -x or --extra_itp      : path to an additional .itp file from outside --ff_path. Optional."
+  echo "                           Can be repeated multiple times."
+  echo ""
+  echo "  --dry-run              : print all inputs, check input_path and ff_path are valid, then exit"
+  echo ""
+  echo "  --notrj                : copy all simulation data except trajectory files. Optional."  
+  echo ""
+  echo "  -h                     : print this message then exit."  
+  echo ""
+  exit 0
+fi
 # check that variables are set
 if [ -z "$input_sysname" ] ; then
   echo "Error: --input_sysname not specified"
@@ -71,6 +109,12 @@ fi
 
 if [ -z "$input_path" ] ; then
   echo "Error: --input_path not specified"
+  exit 1
+fi
+
+# Check that input path folder exists
+if [ ! -d "$input_path" ]; then
+  echo "Error: --input_path folder '$input_path' not found"
   exit 1
 fi
 
@@ -85,16 +129,13 @@ if [ -z "$ff_path" ] ; then
   exit 1
 fi
 
-# Check that forcefield_folder exists
+# Check that forcefield folder exists
 if [ ! -d "$ff_path" ]; then
-  echo "Error: forcefield_folder '$ff_path' does not exist"
+  echo "Error: --ff_path folder '$ff_path' not found "
   exit 1
 fi
 
-
-# build the 
-
-
+# print output
 
 # Output the values of the variables
 echo "Input sysname: $input_sysname"
@@ -115,7 +156,10 @@ fi
 # if [ "$trj" = true ]; then
 #   echo "--trj specified; trajectory files will be copied"
 # fi
-echo
+
+# do the damn thing
+
+echo ""
 echo "Beginning system copy"
 
 if [ -d "$output_sysname" ]; then
